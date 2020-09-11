f7faad243e8d5bb641c6f21598bccb0164166865
Tiran Parazyan <tiran.parazyan@moacommunications.com>
2020-09-11 02:39:44 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <h1>SSSSSSSS</h1>
+        <iframe src="http://localhost:5000/" style={{ width: '100%' }} allowFullScreen/>
+        <iframe src="http://cwer.ws/" style={{ width: '100%' }} allowFullScreen/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
